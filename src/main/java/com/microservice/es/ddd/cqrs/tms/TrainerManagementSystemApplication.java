08b669288d2889520cfe9d7ed82899e5a530c921
Shan <info@telekomatrix.com>
2017-04-26 19:25:32 +0100

    Initiated skeleton

---
+++ src/main/java/com/microservice/es/ddd/cqrs/tms/TrainerManagementSystemApplication.java
@@ -0,0 +1,12 @@
+package com.microservice.es.ddd.cqrs.tms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainerManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainerManagementSystemApplication.class, args);
+	}
+}
