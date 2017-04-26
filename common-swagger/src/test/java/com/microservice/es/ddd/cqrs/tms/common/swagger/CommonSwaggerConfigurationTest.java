9b4c04cd24a7a4a06a2b6ae1527fd18f026e95bd
Muthhus <shan@telekomatrix.com>
2017-04-26 22:21:23 +0100

    Application Swgger configuration set.

---
+++ common-swagger/src/test/java/com/microservice/es/ddd/cqrs/tms/common/swagger/CommonSwaggerConfigurationTest.java
@@ -0,0 +1,38 @@
+package com.microservice.es.ddd.cqrs.tms.common.swagger;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class CommonSwaggerConfigurationTest 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public CommonSwaggerConfigurationTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( CommonSwaggerConfigurationTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
