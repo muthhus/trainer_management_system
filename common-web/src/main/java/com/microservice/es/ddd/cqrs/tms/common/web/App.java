c2b855827e94ca6ddd369f6ae1600e7fe1add144
Muthhus <shan@telekomatrix.com>
2017-04-27 18:37:55 +0100

    DDD, CQRS and ES Skeleton impl

---
+++ common-web/src/main/java/com/microservice/es/ddd/cqrs/tms/common/web/App.java
@@ -0,0 +1,13 @@
+package com.microservice.es.ddd.cqrs.tms.common.web;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
